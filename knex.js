4682dd84c81c6f6c4c9178f3481c6d69f1b293d8
Edgar Chirivella <edgar.95.12@gmail.com>
2018-12-17 17:00:27 +0100

    Add Knex setup

---
+++ knex.js
@@ -0,0 +1,8 @@
+const environment = process.env.ENVIRONMENT || 'development'
+const config = require('./knexfile.js')[environment];
+
+const knex = require('knex')(config);
+const setupPaginator = require('knex-paginator');
+setupPaginator(knex);
+
+module.exports = knex;
